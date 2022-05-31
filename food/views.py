995a21b8062f22b3585190a078dff6be0d7755b0
Katrina Hill <khill365@gmail.com>
2022-05-31 15:13:13 -0700

    updated urls.py and views.py

--- food/views.py
+++ food/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import Food
 from .serializers import FoodSerializer
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 
@@ -11,5 +12,11 @@ class FoodList(generics.ListCreateAPIView):
 
 
 class FoodDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
+
+
+class FoodDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
